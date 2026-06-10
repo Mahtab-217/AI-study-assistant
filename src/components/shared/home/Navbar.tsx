ba7370fbbfc0806b5ec77ba275b6a7c9fa984720
Mahtab-217 <mahtabnasiri120089@gmail.com>
2026-06-10 21:58:42 +0430

    new codes added

--- src/components/shared/home/Navbar.tsx
+++ src/components/shared/home/Navbar.tsx
@@ -1,7 +1,10 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 
 function Navbar() {
+    const [isOpen, setIsOpen]=useState(false);
     const links: {id: number, link: string, name:string}[]= [
         {
         id:1,
@@ -20,8 +23,9 @@ function Navbar() {
         },
     ]
   return (
+
     <div>
-   <nav className="py-3 px-4 w-full  max-w-7xl mx-auto flex bg-primary rounded-full fixed justify-between items-center ">
+   <nav className={`py-3 px-4 w-full  max-w-7xl mx-auto flex ${isOpen? "bg-primary/60 top-0 rounded-none": "bg-primary top-1.5 rounded-full"} rounded-full top-1.5 left-1/2 -translate-x-1/2 fixed justify-between items-center `}>
     <div className="flex space-x-1.5 items-center">
         <img src="./images/logo.jpg" className="w-8 h-8"  />
         <span className="text-md font-inter hidden md:inline">Study Assistant</span>
@@ -39,21 +43,32 @@ function Navbar() {
     })}
    </div>
 
-   {/* Mobile Nav */}
   {/* Bars Icon */}
+  <div className="md:hidden">
+    {isOpen? <X onClick={()=>setIsOpen(!isOpen)} size={24}/>: <Menu onClick={()=>setIsOpen(!isOpen)} size={24}/>}
+  </div>
+
   
-   <div className="w-full hidden  bg-white/60 pb-4 backdrop-blur-md">
-   <div className="flex flex-col gap-4">
+
+    </nav>
+
+
+
+ {/* Mobile Nav */}
+   <div className={`w-full top-10 fixed transition-all duration-300 left-1/2 -translate-x-1/2  min-h-screen ${isOpen? "flex": "hidden"}  bg-white/50 pb-4 backdrop-blur-md`}>
+
+   <div className="flex flex-col gap-4 items-center w-full ">
+   
     {links.map((y)=>{
         return (
-            <Link to={y.link} key={y.id}>
+            <Link className="hover:text-blue-600 transition-colors duration-300" to={y.link} key={y.id}>
             {y.name}
             </Link>
         )
     })}
    </div>
    </div>
-    </nav>
+
    </div>
   )
 }
